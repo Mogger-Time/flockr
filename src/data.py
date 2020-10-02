7a213d57b6b350131fa6778f24c344a8a9bef51c
Daniel Ouyang <d.ouyang.1@student.unsw.edu.au>
2020-10-02 12:31:44 +1000

    Update data.py

--- src/data.py
+++ src/data.py
@@ -1,15 +1,10 @@
 data = {
-    "accounts": [
-        {
-            # "email" : "registered@gmail.com",
-            # "password" : "validpassword123",
-            # "token" : "registered@gmail.com",
-        }
-    ],
     "users": [
         {
             # "u_id" : 1, 
             # "email" : "registered@gmail.com", 
+            # "password" : "validpassword123",
+            # "token" : "registered@gmail.com",
             # "name_first" : "New", 
             # "name_last" : "User", 
             # "handle_str" : "newuser",
